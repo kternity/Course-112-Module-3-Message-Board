a6b9c39893db069a9f5e2d8247208faf344232b6
Kenneth Chung <kternity@gmail.com>
2023-02-13 20:37:33 -0800

    Course 112 Module #3 Message Board

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+   title = models.CharField(max_length=128)
+   subtitle = models.CharField(max_length=128)
+   body = models.TextField()
+   created_on = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+      return self.title
+   
